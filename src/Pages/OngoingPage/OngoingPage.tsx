b9257cae48a752dae5451c888fd5804fbaaa0277
sarah <sarah@solidware.io>
2020-10-13 20:30:46 +0900

    💄 Update OngoingPage styles

--- src/Pages/OngoingPage/OngoingPage.tsx
+++ src/Pages/OngoingPage/OngoingPage.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 
+import * as styles from './OngoingPage.styles';
+
 export default function OngoingPage() {
   return (
-    <div>
-      <li>vote 01</li>
-      <li>vote 02</li>
-      <li>vote 03</li>
-      <li>vote 04</li>
-      <li>vote 05</li>
+    <div className={styles.Root}>
     </div>
   );
 }
